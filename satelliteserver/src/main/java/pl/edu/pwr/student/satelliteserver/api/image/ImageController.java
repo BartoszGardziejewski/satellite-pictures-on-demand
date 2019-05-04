b2d4e9c584a749252f3e36f1cc62079fd6db70dd
pawelzaborowski <pz.zaborowski@gmail.com>
2019-05-04 11:39:26 +0200

    code refactor

--- satelliteserver/src/main/java/pl/edu/pwr/student/satelliteserver/api/image/ImageController.java
+++ satelliteserver/src/main/java/pl/edu/pwr/student/satelliteserver/api/image/ImageController.java
@@ -1,7 +1,6 @@
 package pl.edu.pwr.student.satelliteserver.api.image;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pwr.student.satelliteserver.api.position.Position;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +16,7 @@ public class ImageController {
     private final ImageServiceAPI imageService;
 
     @GetMapping(value = "satellite/api/image/now")
-    public String getCurrentImage() throws IOException {
+    public String getCurrentImage() {
 
         return this.imageService.getSatelliteImage();
     }
@@ -26,7 +24,7 @@ public class ImageController {
     @GetMapping("satellite/api/image")
     public @ResponseBody ImageDaysWrapper getImageAtPosition(@RequestParam Double latitude,
                                                              @RequestParam Double longitude,
-                                                             HttpServletResponse response) throws IOException{
+                                                             HttpServletResponse response) {
 
         if(latitude > 90.0 || latitude < -90.0 || longitude > 180.0 || longitude < -180.0){
             response.setStatus( HttpServletResponse.SC_BAD_REQUEST  );
@@ -34,17 +32,5 @@ public class ImageController {
         }
 
         return this.imageService.getImageAtPosition(new Position(latitude, longitude));
-    }
-
-    private byte[] processByteArray(Byte[] imageFromService){
-
-        byte[] byteArray = new byte[imageFromService.length];
-
-        int i = 0;
-        for (byte wrappedByte : imageFromService) {
-            byteArray[i++] = wrappedByte;
-        }
-
-        return byteArray;
     }
 }
